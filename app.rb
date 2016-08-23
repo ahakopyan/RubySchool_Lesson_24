ac9f384b2ee3754fcdf6e2eafed5868cff07af22
Араик Акопян <araikakopan@kab130.sdm.local>
2016-08-23 23:14:43 +0300

    Initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+ #encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit 
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+	erb "Ok! Ваше имя:ß #{@username}, ваш номер телефона: #{@phone}, время записи: #{@datetime}, ваш мастер: #{@barber} , Цвет волос: #{@color}"
+end
